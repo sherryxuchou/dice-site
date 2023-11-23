fdcd3e3936a0c9014b6b884006d2cb82737c8ee3
sherryxuchou <123006713+sherryxuchou@users.noreply.github.com>
2023-11-22 17:17:20 -0800

    text generation and adding the buttons

--- scripts.js
+++ scripts.js
@@ -54,7 +54,10 @@ const imageArray = [
     "images/dice/dice-34.png",
     "images/dice/dice-35.png",
     "images/dice/dice-36.png",
-    "images/dice/dice-37.png"
+    "images/dice/dice-37.png",
+    "images/dice/dice-38.png",
+    "images/dice/dice-39.png",
+    "images/dice/dice-40.png"
 ];
 
 const diceImages = [
@@ -67,6 +70,7 @@ const diceImages = [
 ];
 
 const button = document.querySelector(".tray-img");
+const outcomeButton = document.querySelector(".refreshOutcome");
 let box_min_x = 350;
 let box_min_y = 0;
 let box_max_x = 610;
@@ -78,6 +82,7 @@ let box_range_y = box_max_y - box_min_y;
 window.onload = () => generateRandomPicture(imageArray, diceImages);
 
 button.addEventListener("click", () => generateRandomPicture(imageArray, diceImages));
+outcomeButton.addEventListener("click", () => spawnOutcome());
 
 function displayText() {
   var text = document.getElementById("textField");
@@ -86,4 +91,54 @@ function displayText() {
     } else {
         text.style.display = "none";
   }
+}
+
+function getRandomSentence () {
+    var index= Math.floor(Math.random() * (sentences.length));
+    return sentences[index];
+}
+
+var sentences= [
+    'you got sat on by a mountain troll',
+    'you got beat up by a goat',
+    'you fell to the bottom of a well',
+    'you tried to summon a powerful creature but summoned a rock instead',
+    'you become possessed by an evil spirit',
+    'you slipped getting out of the tub, faceplanted, and died',
+    'a cat scratched your entire face off',
+    'the potion you were making exploded in your face',
+    'you were killed by 10 frightened cows',
+    'the townspeople burnt you at the stake',
+    'you tripped over a root and drowned in a puddle',
+    'you fell down a hole and encountered a minotaur',
+    'you tried to tame a dragon. it did not work',
+    'is like Bazooka Joe, 5 cents a blow',
+    'is like an ATM, open 24/7',
+    'is like a championship ring, everybody puts a finger in her'
+];
+
+function scribble(){
+  // get the canvas element you want to write to
+  var canvas = document.getElementById("woodcut");
+  // get a handle for the above canvas (here 2d only for simple text)
+  var context = canvas.getContext("2d");
+  // the canvas is blank the first time only, so erase the content
+  // even if it is already blank, checking for it would be more
+  // complicated and slower, too
+  context.clearRect(0, 0, canvas.width, canvas.height);
+  // choose a font (you can choose the size also as you can see)
+  context.font = "30px Arial";
+  // the letters are filled and they are filled in red
+  context.fillStyle = "red";
+  // center the text horizontally
+  context.textAlign = "center";
+  // put a random line in the middle of the canvas
+  // the +10 account for the fonttype's height
+  context.fillText(getRandomSentence(),0, canvas.height/2 + 10);
+}
+
+function spawnOutcome() {
+    var outcome = document.getElementById("sentences")
+    console.log("hi")
+    outcome.innerText = getRandomSentence();
 }
